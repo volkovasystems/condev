b45a474d48e7fc9c7bb7e1bfc054b2e98605c368
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-12 12:09:33 +0800

    Updated test.

--- condev-test.js
+++ condev-test.js
@@ -9,14 +9,18 @@ assert.equal( condev( "yeah", String ), true, "should be true" );
 
 assert.equal( condev( "yeah", function hello( ){ } ), false, "should be false" );
 
-assert.equal( condev( "yeah", function condition( entity ){ return entity == "yeah"; } ), true, "should be true" );
+assert.equal( condev( "yeah", function condition( entity ){
+return entity == "yeah"; } ), true, "should be true" );
 
-assert.equal( condev( "yeah", function condition( entity ){ return typeof entity == "number"; } ), false, "should be false" );
+assert.equal( condev( "yeah", function condition( entity ){
+return typeof entity == "number"; } ), false, "should be false" );
 
 assert.equal( condev( "yeah", /eah/ ), true, "should be true" );
 
-assert.equal( condev( "yeah", ( entity ) => ( typeof entity == "string" ) ), true, "should be true" );
+assert.equal( condev( "yeah", ( entity ) => ( typeof entity == "string" ) ),
+true, "should be true" );
 
-assert.equal( condev( function constructor( ){ }, "constructor" ), true, "should be true" );
+assert.equal( condev( function constructor( ){ }, "constructor" ),
+true, "should be true" );
 
 console.log( "ok" );
