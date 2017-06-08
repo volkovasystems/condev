05ca1c31bfb8b8e409123a36b9607cac9c06e173
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-08 20:02:59 +0800

    Updated test.

--- condev-test.js
+++ condev-test.js
@@ -2,19 +2,55 @@
 const assert = require( "assert" );
 const condev = require( "./condev.js" );
 
-assert.equal( condev( null, OBJECT, true ), false, "should be false" );
-
 assert.equal( condev( null, OBJECT, false ), true, "should be true" );
+
+assert.equal( condev( "test", "test" ), true, "should be true" );
 
 assert.equal( condev( "yeah", String ), true, "should be true" );
 
-assert.equal( condev( "yeah", function hello( ){ } ), false, "should be false" );
+assert.equal( condev( true, Boolean ), true, "should be true" );
 
-assert.equal( condev( "yeah", function condition( entity ){ return entity == "yeah"; } ), true, "should be true" );
+assert.equal( condev( 123, Number ), true, "should be true" );
 
-assert.equal( condev( "yeah", function condition( entity ){ return typeof entity == "number"; } ), false, "should be false" );
+assert.equal( condev( 0, 0, false ), true, "should be true" );
+
+assert.equal( condev( 0, 0, true ), true, "should be true" );
+
+assert.equal( condev( function( ){ }, function( ){ } ), true, "should be true" );
+
+assert.equal( condev( function Hello( ){ return "hello" }, function Hello( ){ return "hi" } ), true, "should be true" );
 
 assert.equal( condev( "yeah", /eah/ ), true, "should be true" );
+
+assert.equal( condev( [ ], Array ), true, "should be true" );
+
+assert.equal( condev( { }, Object ), true, "should be true" );
+
+assert.equal( condev( [ ], Object ), true, "should be true" );
+
+assert.equal( condev( Array, "Array" ), true, "should return true" );
+
+assert.equal( condev( Array, Array ), true, "should return true" );
+
+assert.equal( condev( Object, Object ), true, "should return true" );
+
+assert.equal( condev( Date, Date ), true, "should return true" );
+
+assert.equal( condev( Array, Object ), true, "should return true" );
+
+assert.equal( condev( Date, Object ), true, "should return true" );
+
+assert.equal( condev( RegExp, Object ), true, "should return true" );
+
+assert.equal( condev( Function, Object ), true, "should return true" );
+
+assert.equal( condev( Array, Function ), true, "should be true" );
+
+assert.equal( condev( Date, Function ), true, "should be true" );
+
+assert.equal( condev( RegExp, Function ), true, "should be true" );
+
+assert.equal( condev( "yeah", function condition( entity ){ return entity == "yeah"; } ), true, "should be true" );
 
 assert.equal( condev( "yeah", ( entity ) => ( typeof entity == "string" ) ), true, "should be true" );
 
@@ -23,6 +59,28 @@ assert.equal( condev( function constructor( ){ }, "constructor" ), true, "should
 assert.equal( condev( null, [ OBJECT, STRING ], false ), true, "should be true" );
 
 assert.equal( condev( "yeah", [ STRING, "yeah" ] ), true, "should be true" );
+
+
+assert.equal( condev( null, OBJECT, true ), false, "should be false" );
+
+assert.equal( condev( { }, { } ), false, "should return false" );
+
+assert.equal( condev( 0, "0" ), false, "should return false" );
+
+assert.equal( condev( 0, "" ), false, "should return false" );
+
+assert.equal( condev( null, undefined ), false, "should return false" );
+
+assert.equal( condev( 123, "123" ), false, "should return false" );
+
+assert.equal( condev( 123, 456 ), false, "should be false" );
+
+assert.equal( condev( function Hello( ){ }, function Hi( ){ } ), false, "should return false" );
+
+assert.equal( condev( "yeah", function hello( ){ } ), false, "should be false" );
+
+assert.equal( condev( "yeah", function condition( entity ){ return typeof entity == "number"; } ),
+				false, "should be false" );
 
 assert.equal( condev( 123, [ STRING, "yeah" ] ), false, "should be false" );
 
